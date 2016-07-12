762054d0d26f2890ebc8c4b43c236ee8866f2a8b
Jatin Sandilya <sandilya.jatin@gmail.com>
2016-07-12 19:41:05 +0530

    Added local mongo

---
+++ database.js
@@ -0,0 +1,20 @@
+var MongoClient = require('mongodb').MongoClient;
+var Promise = require('bluebird');
+//var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://jats22:jpg308@ds021289.mlab.com:21289/empfeed';
+module.exports = {
+	connect: function() {
+		return new Promise(function(resolve, reject) {
+			MongoClient.connect(url, function(err, db) {
+				if(err) {
+					return reject(err);
+				}
+
+				return resolve(db);
+			});
+		});
+	},
+	close: function(db) {
+		db.close();
+	}
+};
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-
+//const MongoClient = require('mongodb').MongoClient;
+//var daasUrl = 'mongodb://jats22:jpg308@ds021289.mlab.com:21289/empfeed';
+var database = require('./database');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,54 +12,75 @@ app.use(express.static('public'));
 
 app.set('view engine','ejs');
 
-var db;
-
-MongoClient.connect('mongodb://jats22:jpg308@ds021289.mlab.com:21289/empfeed',function(err,database){
-	if(err) return console.log(err);
-	db = database;
-	app.listen(3000,function(){
-		console.log("Listening on 3000");
-	})
-})
+// MongoClient.connect('mongodb://jats22:jpg308@ds021289.mlab.com:21289/empfeed',function(err,database){
+// 	if(err) return console.log(err);
+// 	db = database;
+	
+// })
 
 app.get('/',function(req,res){
-	db.collection('quotes').find().toArray(function(err,results){
-		if(err) return console.log(err);
+	console.log('Get request')
+	
+	database.connect().then(function(db){
+		var collection = db.collection('quotes');
+		collection.find().toArray(function(err,quote){
+			res.render('index.ejs',{quotes: quote});
+		})
+	})
 
-		console.log(results);
+
 		
-		res.render('index.ejs',{quotes :results});
-	});
-	//res.sendFile(__dirname+'/index.html');
+		
+	// });
 })
 
 app.post('/quotes',function(req,res){
-	console.log(req.body);
-	db.collection('quotes').save(req.body,function(err,result){
-		if(err) return console.log(err);
+	
+	console.log("Post request");
 
-		console.log('Saved to Database');
-		res.redirect('/');			
+	database.connect().then(function(db){
+		db.collection('quotes').insert(req.body,function(err,result){
+			if(err) return res.send(err);
+			res.redirect('/');
+			console.log('Saved to Database');
+
+			database.close(db);
+		})
 	})
+	
 })
 
 app.put('/quotes',function(req,res){
 	console.log("Put request");
-
-	db.collection('quotes')
-	.findOneAndUpdate(
-		{name:'Yoda'},
-		{ $set:{
+	database.connect().then(function(db){
+		db.collection('quotes').findOneAndUpdate(
+		{
+			name:'Yoda'
+		},
+		{ 
+			$set:
+			{
 			name: req.body.name,
 			quote: req.body.quote
-		}},
+			}
+		},
 		{
 			sort: {_id:-1},
 			upsert: true
 		},
 		function(err,result){
 			if(err) return res.send(err)
-				res.send(result)
+			
+			console.log('invasion!');
+			res.redirect('/');
+			database.close(db);
 		}
-	)	
+		)
+
+	})
+	
+})
+
+app.listen(3000,function(){
+		console.log("Listening on 3000");
 })
